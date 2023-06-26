07d24da7908c8f4ee751709f7e277ff24d36c72c
shaolongcai <634047622@qq.com>
2023-06-26 11:46:09 +0800

    完成对后端的参数对接

--- src/components/GenerateFloat.jsx
+++ src/components/GenerateFloat.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState, useContext, useEffect, useCallback } from "react";
+import React, { useState, useContext, useEffect, useRef } from "react";
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -8,27 +8,36 @@ import { globalContext } from '../app/layout';
 const EventSource = require('eventsource');
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm'
+import styles from '../styles/GenerateFloat.module.css'
 
 function GenerateFloat(props) {
 
     const { topic, open, onClose } = props
 
-    const [generateText, setGenerateText] = useState('')
+    const [message, setMessage] = useState('')
     const [value, setValue] = useState('按照场景生成流程')
-    const isGenerate = Boolean(generateText)
+    const isGenerate = Boolean(message)
     const context = useContext(globalContext);
     const [loading, setLoading] = useState(false)
+    const messagesEndRef = useRef(null);
 
     //调用接口生成文本,文本要顶格，否则无法解析markdown
     const handleGenerate = () => {
+
+        const queryParams = new URLSearchParams({
+            scene: topic, //主题
+            tips: value, //额外要求
+            openid: '123'
+        });
+
         if (loading) return
         setLoading(true)
         // 创建一个新的AbortController
         const controller = new AbortController();
         const signal = controller.signal;
-        const source = new EventSource('https://service-ghvb1lfs-1314154270.jp.apigw.tencentcs.com/release/get_llm', { signal });
+        const source = new EventSource(`https://service-ghvb1lfs-1314154270.jp.apigw.tencentcs.com/release/get_llm?${queryParams}`, { signal });
         // 设置重连时间（毫秒）
-        source.reconnectInterval = 30000; 
+        source.reconnectInterval = 30000;
         let msg = ''
         source.onmessage = (event) => {
             // console.log('Received message:', event.data);
@@ -38,7 +47,7 @@ function GenerateFloat(props) {
             const answer = decodeURIComponent(data.answer.replace(/\uff1f/g, '?'));
             console.log('回答：', answer)
             msg += answer
-            setGenerateText(msg)
+            setMessage(msg)
         };
 
         source.onerror = (error) => {
@@ -50,7 +59,7 @@ function GenerateFloat(props) {
 
     useEffect(() => {
         return () => {
-            setGenerateText('')
+            setMessage('')
         }
     }, [topic])
 
@@ -58,11 +67,18 @@ function GenerateFloat(props) {
     const handleDraw = () => {
         const msg = {
             type: 'generateText',
-            text: generateText,
+            text: message,
             name: topic,
         }
         context.postMsg(msg)
     }
+
+    //滚动到底部
+    useEffect(() => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [message]);
 
     return <Drawer classes={{
         paper: "w-[400px] bg-bg", // 使用tailwind复写MUI组件样式的办法
@@ -71,10 +87,11 @@ function GenerateFloat(props) {
         open={open}
         onClose={onClose}>
         <div className="flex h-12 p-4  w-80  text-titleMedium">{topic}</div>
-        <div className="p-4  w-fit overflow-y-auto   min-h-[69.6%] whitespace-pre-wrap text-bodySmall">
-            <ReactMarkdown remarkPlugins={[remarkGfm]} >
-            {generateText}
+        <div className="p-4  w-fit overflow-y-auto min-w-[400px]   min-h-[69.6%] whitespace-pre-wrap text-bodySmall">
+            <ReactMarkdown remarkPlugins={[remarkGfm]} className={styles.markdown} >
+                {message}
             </ReactMarkdown>
+            <div style={{ height: '0px' }} ref={messagesEndRef} ></div>
         </div>
         <TextField
             label="额外要求"
